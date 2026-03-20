d3d11a2a53f318f51fc290fa649b78a98cea76a4
Nebz001 <tanbm@students.nu-lipa.edu.ph>
2026-03-21 04:10:08 +0800

    added routes for register and activity calendar

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/organizations/register', function () {
+  return view('organizations.register');
+});
+
+Route::get('/organizations/activity-calendar', function () {
+  return view('organizations.activity-calendar');
 });
